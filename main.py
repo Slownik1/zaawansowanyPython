a660977e6051c09ded8ca1cbb11b04fabddee675
dawid <dawid1bialek@gmail.com>
2024-03-04 21:31:52 +0100

    python_typing pt1

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def say_hi(name, surname):
+    return f"Cześć {name} {surname}"
 
+def multiply(x, y):
+    return x*y
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def is_even(number):
+    return number%2==0
 
+def chack_triangle(x, y, z):
+    return x+y>=z
 
-# Press the green button in the gutter to run the script.
+def check_list(list, value):
+    return value in list
+
+def marge_list(list1, list2):
+    list_tmp = list1+list2
+    list_tmp=list(set(list_tmp))
+    list_tmp = [element ** 3 for element in list_tmp]
+    return list_tmp
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    say_hi_result = say_hi('Dawid', 'Białek')
+    print(say_hi_result)
+
+    print(multiply(2, 4))
+
+    is_even_result = is_even(2)
+    if(is_even_result):
+        print("Parzysta")
+    else:
+        print("Nieparzysta")
+
+    print(chack_triangle(3,2,6))
+
+    list1=[2,3,4,5,6]
+    print(check_list(list1, 10))
+
+    list2=[2, 5, 6, 3]
+    print(marge_list(list1, list2))
