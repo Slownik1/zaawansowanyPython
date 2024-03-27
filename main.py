33601a9b8d6615a1575586e3df880ff90e02d758
dawid <dawid1bialek@gmail.com>
2024-03-27 22:04:37 +0100

    fix to PEP8

--- main.py
+++ main.py
@@ -7,27 +7,30 @@ def display_name(name):
 
     print(name_lista)
 
+
 def display_number(numbers):
 
     for n in numbers:
-        if n%2==0:
+        if n % 2 == 0:
             print(n)
+
 
 def display_number_2(numbers):
 
     print(numbers[::2])
 
+
 if __name__ == '__main__':
 
     #Zadanie 1
 
-    name =['Dawid', 'Bartek', 'Ala', 'Kasia', 'Ania']
+    name = ['Dawid', 'Bartek', 'Ala', 'Kasia', 'Ania']
 
     #display_name(name)
 
     #Zadanie 2
 
-    numbers=list(range(5))
+    numbers = list(range(5))
     print(numbers)
 
     #display_number(numbers)
