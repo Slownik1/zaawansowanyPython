f85e1484a63ecf341662188239f17a074f305a94
dawid <dawid1bialek@gmail.com>
2024-03-27 21:59:13 +0100

    fix to PEP8

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ import argparse
 import requests
 from brawery import Brawery
 
+
 def say_hi(name, surname):
     return f"Cześć {name} {surname}"
+
 
 def multiply(x, y):
     return x*y
 
+
 def is_even(number):
     return number%2==0
+
 
 def chack_triangle(x, y, z):
     return x+y>=z
 
+
 def check_list(list, value):
     return value in list
+
 
 def marge_list(list1, list2):
     list_tmp = list1+list2
     list_tmp=list(set(list_tmp))
     list_tmp = [element ** 3 for element in list_tmp]
     return list_tmp
+
 
 def getBrawery(city):
 
@@ -35,13 +42,14 @@ def getBrawery(city):
 
     response = requests.get(URL)
 
-    if(response.status_code==200):
+    if (response.status_code==200):
         data = response.json()
         list_of_brawery = [Brawery(brawery) for brawery in data]
         for brawery in list_of_brawery:
             print(brawery)
     else:
         print("Error")
+
 
 if __name__ == '__main__':
 
@@ -58,14 +66,14 @@ if __name__ == '__main__':
     print(multiply(2, 4))
 
     is_even_result = is_even(2)
-    if(is_even_result):
+    if (is_even_result):
         print("Parzysta")
     else:
         print("Nieparzysta")
 
-    print(chack_triangle(3,2,6))
+    print(chack_triangle(3, 2, 6))
 
-    list1=[2,3,4,5,6]
+    list1=[2, 3, 4, 5, 6]
     print(check_list(list1, 10))
 
     list2=[2, 5, 6, 3]
